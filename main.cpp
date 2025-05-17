a1e2c841b89780441fda25a9d0a4cf03688dfd4e
NikBomb <nicola.bombace@gmail.com>
2025-05-17 22:27:44 +0100

    derivatives fd

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <cmath>
 #include <iostream>
+#include <functional>
 
 constexpr double EPS = 0.0000001; 
 
@@ -22,7 +23,7 @@ class PINN{
         std::generate(m_weights.begin(), m_weights.end(), [&](){ return dis(gen); });; 
     }
     
-    double evaluate(double x){
+    double evaluate(double x) const{
         std::vector<double> acc(10,0.0);
         for (int i=0; i < m_hweights.size(); ++i) {
             acc[i] = std::tanh(m_hweights[i] * x + m_hbiases[i]);
@@ -47,11 +48,12 @@ class PINN{
         m_weights[idx] = value;
     }
 
-    double firstDerivativeWrtInput_FD(double x)
-    {
-        double out1 = evaluate(x);
-        double out2 = evaluate(x + EPS);
-        return (out2 - out1)/ EPS; 
+    double getWeight(size_t idx) {
+        return m_weights[idx];
+    }
+
+    double getWeightSize() const {
+        return m_weights.size();
     }
 
     double DerivativeWrtInput(double x)
@@ -59,22 +61,63 @@ class PINN{
         double res = 0.0;
         for (size_t i = 0; i < m_hweights.size(); ++i){
             double z_i = m_hweights[i] * x + m_hbiases[i];
-            res += m_weights[i] * m_hweights[i] * first_deriv_tanh(z_i);
+            res += m_weights[i] * m_hweights[i] * n_deriv_tanh(1, z_i);
         } 
         return res;
     }
 
     private:
-    double first_deriv_tanh(double x){
-        return 1 - std::tanh(x) * std::tanh(x);
-    } 
+    
+
+    double n_deriv_tanh(size_t order, double x){
+        return finite_diff_recursive([](double x){return std::tanh(x);}, x, order);
+    }
+
+    double finite_diff_recursive(std::function<double(double)> fn, double x, int n) {
+        if (n == 0) {
+            return fn(x);
+        }
+        return (finite_diff_recursive(fn, x + EPS , n - 1) - finite_diff_recursive(fn, x , n - 1)) / (EPS);
+    }
 };
+
+namespace finite_difference {
+
+    std::vector<double> n_derivative_num (std::function<std::vector<double>(std::vector<double>)> fn, size_t n) {
+        
+    }
+
+    double firstDerivativeWrtInput(const PINN& pinn, double x){
+        double out = pinn.evaluate(x);
+        double out1 = pinn.evaluate(x + EPS);
+        return (out1 - out) / EPS;
+    }
+
+    std::vector<double> firstDerivativeWrtWeights(const PINN& pinn, double x){
+        std::vector<double> out(pinn.getWeightSize(), 0.0);
+
+        double eval = pinn.evaluate(x);
+
+        for (int i =0; i < pinn.getWeightSize(); ++i){
+            PINN p = pinn;
+            p.setWeight(i, p.getWeight(i) + EPS);
+            double new_eval = p.evaluate(x);
+            out[i] = (new_eval - eval)/EPS;
+        }
+        return out;
+    }
+}
 
 int main(){
     PINN pinn{};
-    double res = pinn.evaluate(3);
-    double num_der = pinn.firstDerivativeWrtInput_FD(3);
-    double der_lag = pinn.DerivativeWrtInput(3);
+    double x = 0.5;
+    double num_der = finite_difference::firstDerivativeWrtInput(pinn, x);
+    double der_lag = pinn.DerivativeWrtInput(x);
+    
+    std::cout << "Numerical first derivative wrt input: " << num_der << "\n";
+    std::cout << "Analytical first derivative wrt input: " << der_lag << "\n";
+    std::cout << "-----------------------------------------" << "\n";
 
-    std::cout << "Evaluates as: " << res << "\n"; 
+
+    
 }
